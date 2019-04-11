0c3a777c768f0fa091171a8967b5e98387889356
Jack Cooper <pog777six@gmail.com>
2019-04-12 03:34:15 +1000

    Initial Commit

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class EnemyController : MonoBehaviour
+{
+    public GameObject projectilePrefab;
+    public TextMeshPro fireText;
+
+    [SerializeField] private float projectileSpeed = 30;
+    [SerializeField] private float fireRate = 0.5f;
+
+    private float fireTimer;
+    private GameObject player;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fireTimer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player != null) {
+            if (fireTimer > 0) {
+                fireTimer -= Time.deltaTime;
+                fireText.text = fireTimer.ToString();
+            }
+            else {
+                Shoot(player);
+                fireTimer = fireRate; 
+            }
+        }
+    }
+
+    private void Shoot(GameObject target) {
+        GameObject projectile = Instantiate(projectilePrefab, gameObject.transform.position, Quaternion.identity);  //spawn projectile
+        Rigidbody2D projectileRb = projectile.GetComponent<Rigidbody2D>();                                          //find projectile rigidbody
+        projectileRb.AddForce((target.transform.position - projectile.transform.position) * projectileSpeed);       //fire towards target
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.tag.Equals("Player")) {    //find and set the player
+            Debug.Log("Enemy Found Player" + collision);
+            player = collision.gameObject;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        if (collision.gameObject.tag.Equals("Player")) {    //stop shooting when player is far enough away
+            Debug.Log("Enemy Lost Player" + collision);
+            player = null;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private GameObject player;
+    private Camera cam;
+
+    [Header("Movement Properties")]
+    [SerializeField] private float speed = 8;           //speed of player movement
+
+    [Header("Scale Properties")]
+    [SerializeField] private float scaleSpeed = 0.1f;   //speed player changes size
+    [SerializeField] private bool minimumSize = false;  //is player the minimum size
+    [SerializeField] private Vector3 size;           //current size
+    [SerializeField] private float timeScale;        //current timeScale
+
+    private Vector3 scaleVector;                     //modifier of scale
+
+    [Header("Combat Properties")]
+    public GameObject projectilePrefab;
+    [SerializeField] private float projectileSpeed = 30;
+    [SerializeField] private float fireRate = 0.5f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        cam = Camera.main;
+        Debug.Log("Camera found: " + cam + cam.name);
+
+        scaleVector.Set(scaleSpeed, scaleSpeed, scaleSpeed);
+        size = player.transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        size = player.transform.localScale;
+        Movement();
+        Size();
+    }
+
+    private void Movement() {
+
+        float xInput = Input.GetAxis("Horizontal");
+        float yInput = Input.GetAxis("Vertical");
+
+        float xVelocity = xInput * speed * Time.deltaTime;
+        float yVelocity = yInput * speed * Time.deltaTime;
+
+        if (xVelocity != 0 || yVelocity != 0) {
+            transform.position += new Vector3(xVelocity, yVelocity, 0);
+        }
+
+        Vector3 position = transform.position;
+        transform.position = position;
+    }
+
+    private void Size() {
+        if(size.x - scaleVector.x <= 0) {       //check if player can shrink anymore
+            minimumSize = true;
+        }
+        else {
+            minimumSize = false;
+        }
+
+        if(Input.GetAxisRaw("Mouse ScrollWheel") < 0) {     //enlarge player
+            player.transform.localScale = size + scaleVector;
+
+            //cam.transform.position.Set(player.transform.position.x, player.transform.position.y, cam.transform.position.z + scaleVector.z);  //please don't remove this, i want to do something with it  -jack
+
+            cam.orthographicSize += scaleVector.x * 5;
+
+            changeTime(size.x);     //modify timescale
+        }
+        else if(Input.GetAxisRaw("Mouse ScrollWheel") > 0 && !minimumSize) {    //shrink player
+            player.transform.localScale = size - scaleVector;
+
+            if (cam.orthographicSize - scaleVector.x * 5 > 0) {     //ensure camera size doesn't become a negative
+                cam.orthographicSize -= scaleVector.x * 5;
+            }
+
+            changeTime(size.x);     //modify timescale
+        }
+    }
+
+    private void changeTime(float timeShift) {
+        if (timeShift > 0.05 && timeShift < 10) {       //maximum time change
+            timeScale = timeShift;                      //to monitor current time scale
+            Time.timeScale = timeScale;                 //set time scale
+        }
+    }
+
+    private void Shoot(GameObject target) {
+        GameObject projectile = Instantiate(projectilePrefab, gameObject.transform.position, Quaternion.identity);  //spawn projectile
+        Rigidbody2D projectileRb = projectile.GetComponent<Rigidbody2D>();                                          //find projectile rigidbody
+        projectileRb.AddForce((target.transform.position - projectile.transform.position) * projectileSpeed);       //fire towards target
+    }
+}
