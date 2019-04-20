c0d23670c0484d4c6037d0e07545af0abe226a40
Emmanuel <12596514@student.uts.edu.au>
2019-04-20 12:13:59 +1000

    Added partially working main menu to enter the game

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    private GameObject mainMenu;
+    private GameObject levelSelect;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainMenu = GameObject.FindWithTag("MainMenu");
+        levelSelect = GameObject.FindWithTag("LevelSelect");
+
+        levelSelect.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NewGame()
+    {
+        mainMenu.SetActive(false);
+        levelSelect.SetActive(true);
+        Debug.Log("Level Select");
+    }
+
+    public void LevelSelect()
+    {
+        SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+        Debug.Log("Load Level");
+    }
+
+    public void ReturnMainMenu()
+    {
+        mainMenu.SetActive(true);
+        levelSelect.SetActive(false);
+        Debug.Log("Back To Main Menu");
+    }
+}
