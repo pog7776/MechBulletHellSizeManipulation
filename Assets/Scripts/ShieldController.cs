b52903a90ab8e3774db868becde296d0bceae462
Jack Cooper <pog777six@gmail.com>
2019-05-25 18:41:23 +1000

    Update ShieldController.cs

--- Assets/Scripts/ShieldController.cs
+++ Assets/Scripts/ShieldController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ShieldController : MonoBehaviour
 {
+    private GameObject projectile;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +15,14 @@ public class ShieldController : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.tag.Equals("Projectile")){
+            projectile = other.gameObject;
+            //projectile.GetComponent<Rigidbody2D>().velocity = new Vector2(shootDirection.x, shootDirection.y).normalized * projectileSpeed;
+            //to do- get velocity from projectile and change the vector 2 to a new direction away from the player
+            //if you want i can do this later - jack
+        }
     }
 }
